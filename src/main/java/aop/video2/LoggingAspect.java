7cd3aba97a3e215b9c42b277ced1f46b46591aa4
AndreyWild <and1rei@mail.ru>
2021-03-22 12:11:07 +0300

    video2 part2

---
+++ src/main/java/aop/video2/LoggingAspect.java
@@ -0,0 +1,15 @@
+package aop.video2;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect // https://mvnrepository.com/artifact/org.aspectj/aspectjweaver
+public class LoggingAspect {
+
+    @Before("execution(public void getBook())") // перед методом ... getBook()
+    public void beforeGetBookAdvice(){
+        System.out.println("beforeGetBookAdvice: попытка получить книгу");
+    }
+}
--- src/main/java/aop/video2/MyConfig.java
+++ src/main/java/aop/video2/MyConfig.java
@@ -2,8 +2,10 @@ package aop.video2;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @ComponentScan("aop.video2")
+@EnableAspectJAutoProxy
 public class MyConfig {
 }
