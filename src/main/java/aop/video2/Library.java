fe1931723dee7356d094b13b817c2ccffd23394a
AndreyWild <and1rei@mail.ru>
2021-03-22 11:53:34 +0300

    video2 part1

---
+++ src/main/java/aop/video2/Library.java
@@ -0,0 +1,11 @@
+package aop.video2;
+
+import org.springframework.stereotype.Component;
+
+@Component("libraryBean")
+public class Library {
+
+    public void getBook(){
+        System.out.println("Мы берем книгу");
+    }
+}
---
+++ src/main/java/aop/video2/MyConfig.java
@@ -0,0 +1,9 @@
+package aop.video2;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("aop.video2")
+public class MyConfig {
+}
